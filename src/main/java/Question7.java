dc99e059dacf6395df59426ddb8b09c295979401
Eronsennies <1143765981@qq.com>
2021-09-22 22:49:47 +0800

    数组中和为0的3个数字

--- src/main/java/Question7.java
+++ src/main/java/Question7.java
@@ -13,13 +13,16 @@ public class Question7 {
         List<List<Integer>> result = new LinkedList<>();
 
         if (nums.length >= 3) {
+            // 首先要对数组进行排序
             Arrays.sort(nums);
 
             int i = 0;
             while (i < nums.length - 2) {
+                // 固定一个值，去寻找和为相反数的两个其他值
                 twoSum(nums, i, result);
                 int temp = nums[i];
 
+                // 去重
                 while (i < nums.length && nums[i] == temp) {
                     i++;
                 }
@@ -38,6 +41,7 @@ public class Question7 {
                       result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                       int temp = nums[j];
 
+                      // 去重
                       while (nums[j] == temp && j < k) {
                           j++;
                       }
@@ -54,3 +58,10 @@ public class Question7 {
         System.out.println(threeSum(nums));
     }
 }
+/*
+    思路：
+    该题的解决办法和Q6类似，输入的数组如果是排序的，就可以先固定一个数字i，然后在排序数组中查找和为-i的两个数字。
+    由于已经有了O(n)时间在排序数组中找出和为定值的两个数字的方法，因此查找三元组的时间复杂度为O(n^2)。
+    但题目并没有说明数组是排序数组，使用Java自带的排序方法的时间复杂度为O(nlogn)，加在一起的时间复杂度仍为O(n^2)。
+    由于要避免重复的三元数组，因此函数twoSum使用一个while循环下标j跳过重复的数字。基于同意的原因，函数threeSum中也要有一个while方法去重。
+ */
